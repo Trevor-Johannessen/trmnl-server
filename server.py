c099b456d99b7db46bdb50d79a0b2b7636e22f23
Trevor Johannessen <tjohannessen@gmail.com>
2026-02-22 00:38:24 -0500

    Added cache to server

--- server.py
+++ server.py
@@ -1,17 +1,29 @@
 from dotenv import load_dotenv
 load_dotenv()
+from contextlib import asynccontextmanager
+from typing import AsyncIterator, Dict, Optional
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi_cache.decorator import cache
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.inmemory import InMemoryBackend
 from typing import Dict, Any
 from datetime import datetime, timedelta
 from screenController import ScreenController
 from init import init_screen
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI) -> AsyncIterator[None]:
+    FastAPICache.init(InMemoryBackend())
+    yield
+
+app = FastAPI(lifespan=lifespan)
 controller = ScreenController(init_screen)
 
 @app.get("/update")
+@cache(namespace="cache", expire=30)
 async def update():
+    print("Update cache expired!")
     return controller.render()
 
 @app.post("/update/ai")
